b4b5c323fdb130f8db32ab1339de701dbd5fd979
huangk <huankai@139.com>
2018-08-27 14:17:29 +0800

    完成 feign 各微服务之间调用的认证

---
+++ hk-app/src/main/java/com/hk/app/rest/TestController.java
@@ -0,0 +1,31 @@
+package com.hk.app.rest;
+
+import com.hk.commons.util.JsonUtils;
+import com.hk.emi.api.domain.City;
+import com.hk.emi.api.feign.CityFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hk.core.authentication.api.SecurityContextUtils;
+import com.hk.core.web.JsonResult;
+
+import java.util.List;
+
+/**
+ * @author kally
+ * @date 2018年8月17日下午1:56:50
+ */
+@RestController
+public class TestController {
+
+    @Autowired
+    private CityFeignClient cityFeignClient;
+
+    @GetMapping({"/", "/test"})
+    public JsonResult test() {
+        List<City> childList = cityFeignClient.getChildList("4028c08162be57660162be5779cb0000");
+        System.out.println(JsonUtils.serialize(childList, true));
+        return JsonResult.success(SecurityContextUtils.getPrincipal());
+    }
+}
