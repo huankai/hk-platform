7412fa68b008c0a15d4e131625b2f75ef09af165
huangkai <huankai@139.com>
2018-12-23 23:24:53 +0800

    添加 zipkin

---
+++ hk-zipkin-server/src/main/java/com/hk/zipkin/sever/ZipkinServerApplication.java
@@ -0,0 +1,20 @@
+package com.hk.zipkin.sever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin2.server.internal.EnableZipkinServer;
+
+/**
+ * @author huangkai
+ * @date 2018-12-23 15:57
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZipkinServer
+public class ZipkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+}
