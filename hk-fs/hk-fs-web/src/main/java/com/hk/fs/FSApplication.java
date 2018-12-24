9979602aa9b4961bde00bc1e84fc44da9343a4ec
huangk <huankai@139.com>
2018-12-24 18:10:44 +0800

    添加 zipkin ，Thumbnails(图片压缩工具)

--- hk-fs/hk-fs-web/src/main/java/com/hk/fs/FSApplication.java
+++ hk-fs/hk-fs-web/src/main/java/com/hk/fs/FSApplication.java
@@ -1,7 +1,9 @@
 package com.hk.fs;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import net.coobird.thumbnailator.Thumbnails;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * FS Start
@@ -9,10 +11,16 @@ import org.springframework.cloud.client.SpringCloudApplication;
  * @author kevin
  * @date 2018-07-13 14:48
  */
-@SpringCloudApplication
+//@SpringCloudApplication
 public class FSApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FSApplication.class, args);
+    public static void main(String[] args) throws IOException {
+//        SpringApplication.run(FSApplication.class, args);
+
+        // 图片压缩
+        Thumbnails.of(new File("C:\\Users\\sjq-278\\Desktop/021411549t5w.jpg"))
+                .scale(0.25)
+                .outputQuality(1)
+                .toFile(new File("C:\\Users\\sjq-278\\Desktop/2.jpg"));
     }
 }
--- hk-zipkin-server/src/main/java/com/hk/zipkin/sever/ZipkinServerApplication.java
+++ hk-zipkin-server/src/main/java/com/hk/zipkin/sever/ZipkinServerApplication.java
@@ -6,9 +6,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import zipkin2.server.internal.EnableZipkinServer;
 
 /**
+ * spring boot 2.X 版本不再推荐我们来自定义 Server 端，使用 docker 部署
+ *
  * @author huangkai
  * @date 2018-12-23 15:57
  */
+@Deprecated
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZipkinServer
@@ -17,4 +20,5 @@ public class ZipkinServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinServerApplication.class, args);
     }
+
 }
