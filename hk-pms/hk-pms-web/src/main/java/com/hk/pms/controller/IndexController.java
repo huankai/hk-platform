b7decf1d2c76b3c3c281da569d25748cb2f7017f
huangkai <huankai@139.com>
2019-06-29 18:02:58 +0800

    修改

--- hk-pms/hk-pms-web/src/main/java/com/hk/pms/controller/IndexController.java
+++ hk-pms/hk-pms-web/src/main/java/com/hk/pms/controller/IndexController.java
@@ -3,8 +3,6 @@ package com.hk.pms.controller;
 import com.hk.commons.JsonResult;
 import com.hk.core.authentication.api.UserPrincipal;
 import com.hk.platform.commons.web.BaseController;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
  * @author huangkai
  * @date 2018-08-14 16:19
  */
-@Api(value = "首页管理")
 @RestController
 public class IndexController extends BaseController {
 
-    @ApiOperation(value = "首页")
     @GetMapping({"/", "/index"})
     public JsonResult<UserPrincipal> index() {
         return JsonResult.success(getPrincipal());
